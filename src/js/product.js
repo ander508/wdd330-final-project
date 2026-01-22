d00ad694143914d42f19a8afdcfd91e64fb231a7
Oniyemmy24 <Oniyemmy24@gmail.com>
2026-01-22 05:56:34 +0100

    Fix product details

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,16 @@
+import { getParam } from "./utils.mjs";
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from './ProductDetails.mjs';
+
 
 const dataSource = new ProductData("tents");
+const productID = getParam("product");
+
+const product = new ProductDetails(productID, dataSource);
+product.init();
+
+console.log(dataSource.findProductById(productId));
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
