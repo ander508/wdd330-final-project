69faee720bca20e3bcb3f255ea6b4e69b1e6e106
Oniyemmy24 <Oniyemmy24@gmail.com>
2026-01-22 06:37:24 +0100

    products details are added

--- src/js/product.js
+++ src/js/product.js
@@ -1,22 +1,26 @@
 import { getParam } from "./utils.mjs";
-<<<<<<< HEAD
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from './ProductDetails.mjs';
-
-=======
-import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
->>>>>>> 4b6ae7e14200ed016ebb1b0152193b0971ba943e
 
-const productId = getParam("product");
 const dataSource = new ProductData("tents");
-const productID = getParam("product");
+const productId = getParam("product");
 
-const product = new ProductDetails(productID, dataSource);
+const product = new ProductDetails(productId, dataSource);
 product.init();
 
 console.log(dataSource.findProductById(productId));
 
-const product = new ProductDetails(productId, dataSource);
-product.init();
+function addProductToCart(product) {
+  setLocalStorage("so-cart", product);
+}
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
+
+// add listener to Add to Cart button
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
